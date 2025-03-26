0ab86b9056eac5627d1d3b12b1e73ded82666c5b
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2025-03-26 10:45:20 +0100

    after lesson on 26.03.25

---
+++ src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+	let todoList = $state(['Erstes Todo', 'Zweites Todo', 'Drittes Todo']);
+
+	let inputValue = $state('');
+
+	function addItem() {
+		if (inputValue != '') {
+			todoList.push(inputValue);
+			inputValue = '';
+		}
+	}
+
+	function keyPressed(event) {
+		if (event.key === 'Enter') {
+			addItem();
+		}
+	}
+</script>
+
+<div class="header">
+	<input onkeypress={keyPressed} id="todo-input" type="text" bind:value={inputValue} />
+	<button onclick={addItem} id="add-button" disabled={inputValue === ""}>Add</button>
+</div>
+
+<div class="todo-list">
+	{#each todoList as todo, i}
+		<div class="todo-item">
+			<input type="checkbox" id="item{i}" />
+			<label for="item{i}">{todo}</label>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.header {
+		display: flex;
+		margin-bottom: 20px;
+	}
+	#todo-input {
+		display: flex;
+		width: 100%;
+		margin-right: 10px;
+	}
+</style>
