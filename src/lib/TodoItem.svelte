15755e6bc098449d6b1375cfb602be9a406c212d
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2025-04-02 15:25:51 +0200

    created component for TodoItem

---
+++ src/lib/TodoItem.svelte
@@ -0,0 +1,28 @@
+<script>
+	let { id, title, completed } = $props();
+</script>
+
+<div class="todo-item">
+	<input type="checkbox" id="item{id}" />
+	<label for="item{id}">{title} ({id})</label>
+</div>
+
+<style>
+	.todo-item {
+		display: flex;
+		align-items: center;
+		/* margin-bottom: 10px; */
+		border-bottom: 1px solid #0005;
+		padding: 10px;
+		border-radius: 0px;
+		background: #f9f9f9;
+	}
+	input[type='checkbox'] {
+		margin: 0 10px 0 0;
+		width: 1.5em;
+		height: 1.5em;
+		background: white;
+		border-radius: 2px;
+		border: 1px solid #555;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,29 @@
 <script>
+	import TodoItem from '$lib/TodoItem.svelte';
+
 	let todoList = $state([
 		{
+			id: 1,
 			title: 'Learn Svelte',
 			completed: false
 		},
 		{
+			id: 2,
 			title: 'Build a Svelte app',
 			completed: false
 		},
 		{
+			id: 3,
 			title: 'Deploy the app',
 			completed: false
 		}
 	]);
+	// get highest id in the list
+	let nextId = Math.max(...todoList.map((todo) => todo.id)) + 1;
+	// if list is empty, set nextId to 1
+	if (isNaN(nextId)) {
+		nextId = 1;
+	}
 
 	let inputField;
 	let inputValue = $state('');
@@ -20,6 +31,7 @@
 	function addItem() {
 		if (inputValue != '') {
 			todoList.push({
+				id: nextId++,
 				title: inputValue,
 				completed: false
 			});
@@ -50,10 +62,7 @@
 
 <div class="todo-list">
 	{#each todoList as todo, i}
-		<div class="todo-item">
-			<input type="checkbox" id="item{i}" />
-			<label for="item{i}">{todo.title}</label>
-		</div>
+		<TodoItem {...todo} />
 	{/each}
 </div>
 
@@ -62,30 +71,25 @@
 		display: flex;
 		margin-bottom: 20px;
 	}
+
 	#todo-input {
 		display: flex;
 		width: 100%;
+		padding: 10px;
+		border: 1px solid #0005;
+		border-radius: 5px;
+		background: #f9f9f9;
+		box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
 		margin-right: 10px;
 	}
 
 	.todo-list {
 		display: flex;
 		flex-direction: column;
+		border: 1px solid #0005;
+		border-radius: 5px;
+		overflow: hidden;
+		box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
 	}
-	.todo-item {
-		display: flex;
-		align-items: center;
-		margin-bottom: 10px;
-	}
-	input[type='checkbox'] {
-		margin: 0 10px 0 0;
-		width: 1.5em;
-		height: 1.5em;
-		background: white;
-		border-radius: 2px;
-		border: 1px solid #555;
-	}
-	/* input[type='checkbox']:checked {
-		background: #abd;
-	} */
+
 </style>
