11b0a06940f37e5f9fd3aa685471fcc398d6482b
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2025-04-08 15:10:18 +0200

    tmp

--- src/lib/ListItem.svelte
+++ src/lib/ListItem.svelte
@@ -6,11 +6,16 @@
 		completed = event.target.checked;
 		console.log(`Todo item ${id} updated: ${completed}`);
 	}
+
+	function deleteItem() {
+		
+	}
 </script>
 
 <div class="todo-item {completed ? 'completed' : ''}">
-	<input type="checkbox" id="item{id}" onchange={checkboxChanged} />
+	<input type="checkbox" id="item{id}" onchange={checkboxChanged} checked={completed}/>
 	<label for="item{id}">{title} ({id})</label>
+	<button onclick={deleteItem}>Delete</button>
 </div>
 
 <style>
@@ -18,10 +23,17 @@
 		display: flex;
 		align-items: center;
 		margin-bottom: 10px;
+		border: 1px solid #ccc;
+		padding: 10px;
+		border-radius: 5px;
 	}
 
 	input[type='checkbox'] {
 		margin-right: 10px;
+	}
+
+	label {
+		flex-grow: 1;
 	}
 
     .completed {
