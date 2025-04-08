17153997b2e6e31eff71d6796d8aa36231c08605
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2025-04-08 14:27:34 +0200

    tmp

--- src/lib/ListItem.svelte
+++ src/lib/ListItem.svelte
@@ -1,8 +1,31 @@
 <script>
-	let { todo, i } = $props();
+	let { id, title, completed = $bindable() } = $props();
+
+	// Function to handle checkbox change
+	function checkboxChanged(event) {
+		completed = event.target.checked;
+		console.log(`Todo item ${id} updated: ${completed}`);
+	}
 </script>
 
-<div class="todo-item">
-	<input type="checkbox" id="item{i}" />
-	<label for="item{i}">{todo}</label>
+<div class="todo-item {completed ? 'completed' : ''}">
+	<input type="checkbox" id="item{id}" onchange={checkboxChanged} />
+	<label for="item{id}">{title} ({id})</label>
 </div>
+
+<style>
+	.todo-item {
+		display: flex;
+		align-items: center;
+		margin-bottom: 10px;
+	}
+
+	input[type='checkbox'] {
+		margin-right: 10px;
+	}
+
+    .completed {
+        text-decoration: line-through;
+        color: gray;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,33 @@
 <script>
 	import ListItem from '$lib/ListItem.svelte';
 
-	let todoList = $state(['Erstes Todo', 'Zweites Todo', 'Drittes Todo']);
+	let todoList = $state([
+		{ id: 1, title: 'Erstes Todo', completed: false },
+		{ id: 2, title: 'Zweites Todo', completed: false },
+		{ id: 4, title: 'Drittes Todo', completed: false }
+	]);
+	// initialize nextID to the highest id in the todoList + 1
+	let nextID = $state(
+		todoList.length > 0 ? Math.max(...todoList.map(todo => todo.id)) + 1 : 1
+	);
 
 	let inputValue = $state('');
 
+	let sortedTodoList = $derived(
+		todoList.sort((a, b) => {
+			if (a.completed === b.completed) {
+				return a.id - b.id;
+			}
+			return a.completed ? 1 : -1;
+		})
+	);
+
 	function addItem() {
 		if (inputValue != '') {
-			todoList.push(inputValue);
+			todoList.push({ id: nextID++, title: inputValue, completed: false });
 			inputValue = '';
 		}
+		console.log(todoList);
 	}
 
 	function keyPressed(event) {
@@ -21,12 +39,12 @@
 
 <div class="header">
 	<input onkeypress={keyPressed} id="todo-input" type="text" bind:value={inputValue} />
-	<button onclick={addItem} id="add-button" disabled={inputValue === ""}>Add</button>
+	<button onclick={addItem} id="add-button" disabled={inputValue === ''}>Add</button>
 </div>
 
 <div class="todo-list">
-	{#each todoList as todo, i}
-		<ListItem todo={todo} i={i}/>
+	{#each sortedTodoList as todo, i}
+		<ListItem id={todo.id} title={todo.title} bind:completed={todo.completed} />
 	{/each}
 </div>
 
